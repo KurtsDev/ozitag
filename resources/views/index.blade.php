4ac3df53e4f8c4758a786b1f8834e67720f89d3a
Konstantin Kurts <kurts.dev@gmail.com>
2020-09-11 21:43:09 +0300

    required crawler

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
-
         <title>Laravel</title>
     </head>
     <body>
